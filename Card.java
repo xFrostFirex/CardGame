a45ea417104b427c51e6bfdf14088021f08f664c
Spencer Roth <froststarz@gmail.com>
2018-11-01 05:59:29 -0600

    ShuffleFunction

--- Card.java
+++ Card.java
@@ -1,4 +1,5 @@
 package CardGame;
+
 //Credit to Nick and Evan for help.
 /**
  * Created by spencer.roth on 10/31/18.
@@ -8,8 +9,7 @@ public class Card {
     private String Suite;
     private String Value;
 
-
-    //Takes in value and suite and uses this.suite/this.value
+    // Takes in value and suite and uses this.suite/this.value
     public Card(String suite, String value) {
 
         setSuite(suite);
@@ -17,29 +17,22 @@ public class Card {
 
     }
 
-
-
-    //sets this.suite
-    public void setSuite(String suite){
+    // sets this.suite
+    public void setSuite(String suite) {
         this.Suite = suite;
     }
 
-    //sets this.Value
-    public void setValue(String value){
+    // sets this.Value
+    public void setValue(String value) {
         this.Value = value;
     }
 
-
-        public String getSuite(){
+    public String getSuite() {
         return Suite;
-        }
-        public String getValue(){
-            return Value;
-        }
+    }
 
-
-
-
-
+    public String getValue() {
+        return Value;
+    }
 
 }
